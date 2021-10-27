8d9ec32dc6f2d339b44272a15fce016e7d743935
Heather King <hkingdevelopment@gmail.com>
2021-10-27 18:09:50 +0100

    routes for subtract, multiply and divide added

--- controllers/controller.py
+++ controllers/controller.py
@@ -7,8 +7,21 @@ def index():
     return "Welcome to the calculator"
 
 @app.route('/add/<num1>/<num2>')
-def addition(num1, num2):
+def addition_route(num1, num2):
     return f'{add(int(num1), int(num2))}'
+
+@app.route('/subtract/<num1>/<num2>')
+def subtract_route(num1, num2):
+    return f'{subtract(int(num1), int(num2))}'
+
+@app.route('/multiply/<num1>/<num2>')
+def multiply_route(num1, num2):
+    return f'{multiply(int(num1), int(num2))}'
+
+@app.route('/divide/<num1>/<num2>')
+def divide_route(num1, num2):
+    return f'{divide(int(num1), int(num2))}'
+
 
 
 
